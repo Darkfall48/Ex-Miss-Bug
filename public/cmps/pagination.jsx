a7ded10d0f896992133a0189d30546853d2abc35
Darkfall48 <sidney@sebban.be>
2023-01-05 01:24:09 +0200

    Update pagination.jsx

--- public/cmps/pagination.jsx
+++ public/cmps/pagination.jsx
@@ -7,11 +7,9 @@ export function Pagination({ currentPage, totalPages = 5, handlePageChange }) {
 
   return (
     <div className="pagination">
-      <button onClick={() => handlePageChange(0)}>First</button>
       <button onClick={() => handlePageChange(-1)}>Prev</button>
       <span className="curr-page-num">{currentPage + 1}</span>
       <button onClick={() => handlePageChange(1)}>Next</button>
-      <button onClick={() => handlePageChange(totalPages - 1)}>Last</button>
     </div>
   )
 }
